23c37832b1c21a749a97d5fd919bab25e55b7d84
Nyasha Asheck Hove <50446026+asheckn@users.noreply.github.com>
2025-02-12 01:54:40 +0200

    Initial commit

---
+++ src/test/java/com/asheck/smatech_payment_service/SmatechPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asheck.smatech_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmatechPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
